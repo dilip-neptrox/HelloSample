31f8791ae146f45da22ff05ee2beadf4f8ec12ca
dilip-neptrox <[dilipchaudhary400@gmail.com]>
2017-09-11 18:19:04 +0545

    file changed222

--- HelloWorld/Controllers/HomeController.cs
+++ HelloWorld/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace HelloWorld.Controllers
 {
-	public class HomeController : Controller
+	//public class HomeController : Controller
 	{
 		public ActionResult Index()
 		{
