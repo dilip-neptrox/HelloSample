1e0bcb37db668faa70aec3719bea3814d3654426
dilip-neptrox <[dilipchaudhary400@gmail.com]>
2017-09-11 17:11:32 +0545

    file changed222

--- HelloWorld/Controllers/HomeController.cs
+++ HelloWorld/Controllers/HomeController.cs
@@ -17,14 +17,14 @@ namespace HelloWorld.Controllers
 		{
 			ViewBag.Message = "Your application description page.";
 
-			//return View();
+			return View();
 		}
 
 		public ActionResult Contact()
 		{
 			ViewBag.Message = "Your contact page.";
 
-			//return View();
+			return View();
 		}
 	}
 }
